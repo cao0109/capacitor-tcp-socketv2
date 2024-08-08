dc9b732cdab41bb007a0ece69bc1eb110333f658
caoh2 <cao970109@gmail.com>
2024-08-08 15:53:53 +0800

    Fix android bug

--- android/src/main/java/com/svend/plugins/tcp/socket/TcpSocketPlugin.java
+++ android/src/main/java/com/svend/plugins/tcp/socket/TcpSocketPlugin.java
@@ -19,20 +19,14 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 
-@CapacitorPlugin(name = "TcpSocket", permissions = {
-        @Permission(
-                alias = "network",
-                strings = { Manifest.permission.ACCESS_NETWORK_STATE }
-        )
-})
+@CapacitorPlugin(name = "TcpSocket", permissions = {@Permission(alias = "network", strings = {Manifest.permission.ACCESS_NETWORK_STATE})})
 public class TcpSocketPlugin extends Plugin {
-
 
     private Socket socket;
     private DataOutputStream mBufferOut;
     private List<Socket> clients = new ArrayList<>();
 
-    @PluginMethod()
+    @PluginMethod
     public void connect(PluginCall call) {
         String ipAddress = call.getString("ipAddress");
 
@@ -43,7 +37,7 @@ public class TcpSocketPlugin extends Plugin {
         Integer port = call.getInt("port", 9100);
 
         try {
-            if (socket != null && socket.isConnected())   {
+            if (socket != null && socket.isConnected()) {
                 socket.close();
             }
             socket = new Socket(ipAddress, port);
@@ -59,10 +53,11 @@ public class TcpSocketPlugin extends Plugin {
         call.resolve(ret);
     }
 
-    @PluginMethod()
+    @PluginMethod
     public void send(final PluginCall call) {
         final Integer client = call.getInt("client", -1);
         final String msg = call.getString("data", "");
+        final String dataType = call.getString("encoding", "utf8"); // 默认使用 utf8
 
         if (client == -1) {
             call.reject("No client specified");
@@ -75,12 +70,27 @@ public class TcpSocketPlugin extends Plugin {
                 try {
                     final Socket socket = clients.get(client);
                     mBufferOut = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+                    byte[] byteArray;
+
+                    switch (dataType) {
+                        case "utf8":
+                            byteArray = msg.getBytes();
+                            break;
+                        case "hex":
+                            byteArray = hexStringToByteArray(msg);
+                            break;
+                        default:
+                            call.reject("Unsupported data type");
+                            return;
+                    }
+
                     if (mBufferOut != null) {
-                        mBufferOut.write(msg.getBytes());
+                        mBufferOut.write(byteArray);
                         mBufferOut.flush();
                     }
 
                     call.resolve();
+
                 } catch (IOException e) {
                     call.reject(e.getMessage());
                 }
@@ -88,9 +98,11 @@ public class TcpSocketPlugin extends Plugin {
         };
 
         Socket socket = clients.get(client);
-        if (!socket.isConnected()) {
+        if (socket == null || !socket.isConnected()) {
             try {
-                socket.close();
+                if (socket != null) {
+                    socket.close();
+                }
             } catch (IOException e) {
                 call.reject("Generic error");
             }
@@ -101,7 +113,20 @@ public class TcpSocketPlugin extends Plugin {
         thread.start();
     }
 
-    @PluginMethod()
+    private byte[] hexStringToByteArray(String s) {
+        int len = s.length();
+        if (len % 2 != 0) {
+            s = "0" + s; // 如果长度是奇数，前面补0
+            len++;
+        }
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character.digit(s.charAt(i + 1), 16));
+        }
+        return data;
+    }
+
+    @PluginMethod
     public void read(final PluginCall call) {
         final Integer client = call.getInt("client", -1);
         final Integer length = call.getInt("expectLen", 1024);
@@ -143,14 +168,14 @@ public class TcpSocketPlugin extends Plugin {
         thread.start();
     }
 
-    @PluginMethod()
+    @PluginMethod
     public void disconnect(PluginCall call) {
         final Integer client = call.getInt("client", -1);
         if (client == -1) {
             call.reject("No client specified");
             return;
         }
-        if (clients.isEmpty())  {
+        if (clients.isEmpty()) {
             call.reject("Socket not connected");
             return;
         }
